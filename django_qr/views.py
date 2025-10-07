44641766cdc4ea488512ae3f1ddec78913790f11
Arpit-Rajawat <Arpitsingh62650@gmail.com>
2025-10-07 20:54:18 +0530

    second commit

---
+++ django_qr/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .forms import QRCodeForm
+
+def generate_qr_code(request):
+
+    if request.method == 'POST':
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            res_name = form.cleaned_data['restaurant_name']
+            url = form.cleaned_data['url']
+
+            
+    else:
+        form = QRCodeForm()
+        context = {
+                'form':form,
+            }
+        return render(request, 'generate_qr_code.html', context)
