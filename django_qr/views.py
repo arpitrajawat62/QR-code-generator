b170e19f81f68af3a81a4f1f8d5cb8087679ab2a
Arpit-Rajawat <Arpitsingh62650@gmail.com>
2025-10-08 05:52:50 +0530

    third commit

--- django_qr/views.py
+++ django_qr/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .forms import QRCodeForm
+import qrcode
+import os
+from django.conf import settings
 
 def generate_qr_code(request):
 
@@ -8,6 +11,21 @@ def generate_qr_code(request):
         if form.is_valid():
             res_name = form.cleaned_data['restaurant_name']
             url = form.cleaned_data['url']
+
+            #Generate QR Code
+            qr = qrcode.make(url)
+            file_name = res_name.replace(" ", "_").lower() + '_menu.png'
+            file_path = os.path.join(settings.MEDIA_ROOT, file_name)
+            qr.save(file_path)
+
+            #image url
+            qr_url = f"{settings.MEDIA_URL}{file_name}"
+
+            context = {
+                'res_name': res_name,
+                'qr_url' : qr_url
+            }
+            return render(request, 'qr_result.html', context)
 
             
     else:
